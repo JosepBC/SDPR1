142823629cb7776d49f2934c20c3b09364951e5c
Josep Bello Curto <bellocontacto@gmail.com>
2021-04-19 18:28:45 +0200

    Testing client

--- Client/client.py
+++ Client/client.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 import xmlrpc.client
 
+def CountingWords(text):
+    #resp = requests.get(url)
+    return len(text.split())
+
 def main():
     proxy = xmlrpc.client.ServerProxy("http://localhost:9000")
-    proxy.add_worker()
-
+    proxy.addWorker()
+    proxy.addWorker()
+    l = proxy.listWorkers()
+    print(l)
+    proxy.submitTask("sad", "CountingWords", "asd")
 
 if __name__ == '__main__':
     main()
